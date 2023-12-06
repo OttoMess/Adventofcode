97dce4c1e36df7c279949415cc878c6381b6c0be
Wouter Broeze <wouter.broeze@demcon.com>
2023-12-06 18:07:10 +0100

    Puzzle 6 done

--- 2023/Puzzle_#6.py
+++ 2023/Puzzle_#6.py
@@ -1,3 +1,5 @@
+import time
+
 EXAMPLE = "Data/Puzzle_#6_example.txt"
 DATA = "Data/Puzzle_#6.txt"
 TEST = "Data/Puzzle_#6_test.txt"
@@ -8,30 +10,40 @@ class Puzzle6:
 
     def __init__(self, path):
         self.data = list()
-        self.time = list()
-        self.distance = list()
-
-        self.seeds_to_soil = None
-        self.soil_to_fertilizer = None
-        self.fertilizer_to_water = None
-        self.water_to_light = None
-        self.light_to_temperature = None
-        self.temperature_to_humidity = None
-        self.humidity_to_location = None
+        self.time = None
+        self.distance = None
 
         self.file_path = path
+        self.collector_1 = 1
+        self.collector_2 = 0
+        self.collector_3 = 0
+        self.collector_4 = 0
 
         self.read_txt()
-        self.find_time_distance()
+        self.find_time_distance_part1()
+        self.part1()
+        print(f"Output part 1 : {self.collector_1}")
+        self.find_time_distance_part2()
+        self.part2()
 
-        print("test")
+        print(f"Output part 2 : {self.collector_2}")
+        print(f"Output part 2 with V2: {self.collector_3}")
+        print(f"Output part 2 with V3: {self.collector_4}")
 
     def read_txt(self):
         with open(self.file_path) as file:
             for line in file:
                 self.data.append(line.strip())
 
-    def find_time_distance(self):
+    def find_time_distance_part2(self):
+        data = self.data
+        time_str = data[0].split(":")[1].strip().split(" ")
+        self.time = int("".join(c for c in time_str if c.isdigit()))
+
+        distance_str = data[1].split(":")[1].strip().split(" ")
+        self.distance = int("".join(c for c in distance_str if c.isdigit()))
+
+    def find_time_distance_part1(self):
         data = self.data
         time_str = data[0].split(":")[1].strip().split(" ")
         self.time = [int(c) for c in time_str if c.isdigit()]
@@ -39,6 +51,68 @@ class Puzzle6:
         distance_str = data[1].split(":")[1].strip().split(" ")
         self.distance = [int(c) for c in distance_str if c.isdigit()]
 
-        print("test")
+    @staticmethod
+    def travelled(play_time, record):
+        distance = [0 for _ in range(play_time+1)]
+        for i in range(play_time+1):
+            press = i
+            speed = press
+            travel_time = play_time - press
+            distance[i] = speed * travel_time
+
+        above_record = [j for j in distance if j > record]
+        amount = len(above_record)
+
+        return distance, amount
+
+    @staticmethod
+    def travelled_v2(play_time, record):
+        above_record = 0
+        for i in range(play_time+1):
+            travel_time = play_time - i
+            distance = i * travel_time
+            if distance > record:
+                above_record += 1
+
+        return above_record
+
+    @staticmethod
+    def travelled_v3(play_time, record):
+        below_record = list()
+        for i in range(play_time+1):
+            travel_time = play_time - i
+            distance = i * travel_time
+            if distance > record:
+                below_record.append(i)
+                break
+
+        for j in range(play_time+1,0,-1):
+            travel_time = play_time - j
+            distance = j * travel_time
+            if distance > record:
+                below_record.append(j+1)
+                break
+
+        return below_record[1] - below_record[0]
+
+    def part1(self):
+        for j, _ in enumerate(self.time):
+            _, f = self.travelled(self.time[j], self.distance[j])
+            self.collector_1 *= f
+
+    def part2(self):
+        start_time = time.time()
+        _, self.collector_2 = self.travelled(self.time, self.distance)
+        print(f"Part 2 took : {round(time.time()-start_time,4)} [sec]")
+
+        start_time = time.time()
+        self.collector_3 = self.travelled_v2(self.time, self.distance)
+        print(f"Part 2 V2 took : {round(time.time()-start_time,4)} [sec]")
+
+        start_time = time.time()
+        self.collector_4 = self.travelled_v3(self.time, self.distance)
+        print(f"Part 2 V3 took : {round(time.time()-start_time,4)} [sec]")
+
 
 Puzzle6(EXAMPLE)
+Puzzle6(DATA)
