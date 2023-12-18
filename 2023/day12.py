6e61a5c709862220f791ae1867b558518b965608
OttoMess <108458116+OttoMess@users.noreply.github.com>
2023-12-18 22:29:45 +0100

    day12 part 1 done

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,4 +1,5 @@
 import time
+# from functools import cache
 import itertools
 
 DATA = "data/day12.txt"
@@ -9,6 +10,8 @@ TEST = "data/day12_test.txt"
 class Puzzle12:
 
     def __init__(self, path):
+        self.fold_groups = None
+        self.springs = None
         self.vents = None
         self.groups = None
         self.file_path = path
@@ -19,8 +22,8 @@ class Puzzle12:
 
         start_time = time.time()
 
-        self.part1()
-        # self.part2()
+        # self.part1()
+        self.part2()
         print(self.file_path)
         print(f"Sum for part 1: {self.part1_collector}")
         # print(f"Sum for part 2: {self.part2_collector}")
@@ -28,7 +31,50 @@ class Puzzle12:
 
     def part1(self):
         self.read_txt()
-        print("test")
+        for j, _ in enumerate(self.vents):
+            self.part1_collector += self.search(self.vents[j], self.groups[j])
+
+    def part2(self):
+        self.read_txt()
+        self.unfold()
+        for j, _ in enumerate(self.springs):
+            self.part1_collector += self.search(self.springs[j], self.fold_groups[j])
+
+    def search(self, vent, group):
+        q = [pos for pos, char in enumerate(vent) if char == "?"]
+
+        group = [int(g) for g in group]
+        sets = sum(group) - vent.count("#")
+        w = [s for s in itertools.combinations(q, sets)]
+        e = [list(i) for i in w]
+
+        print(len(e))
+
+        ans = 0
+        for j, c in enumerate(e):
+            test = list(vent.replace("?", "."))
+            for i in c:
+                test[i] = "#"
+            valid = self.is_valid("".join(i for i in test), group)
+            if valid:
+                # print("".join(i for i in test))
+                ans += 1
+        print(vent, group, ans)
+        return ans
+
+    @staticmethod
+    def is_valid(vent: str, group: list):
+        vents = [v for v in vent.split(".") if v != ""]
+
+        if len(group) != len(vents):
+            return False
+        elif sum(group) != vent.count("#"):
+            return False
+
+        for j, i in enumerate(vents):
+            if not i.count("#") == group[j]:
+                return False
+        return True
 
     def read_txt(self):
         raw = list()
@@ -43,5 +89,15 @@ class Puzzle12:
         self.groups = groups
         self.vents = vents
 
+    def unfold(self):
+        vents = [(i.split(" ")[0] + "?") * 4 + i.split(" ")[0] for i in self.data]
+        groups = [(i.split(" ")[1]+",") * 4 + i.split(" ")[1]for i in self.data]
+
+        r = [i.split(",") for i in groups]
+
+        self.springs = vents
+        self.fold_groups = r
+
 
 Puzzle12(EXAMPLE)
+# Puzzle12(DATA)
