a127d52c9daa9046bd9f4864d76f8c986e009223
Wouter Broeze <wouter.broeze@demcon.com>
2024-11-04 09:39:27 +0100

    made day4 into class

--- 2023/c++/c_lib.h
+++ 2023/c++/c_lib.h
@@ -2,33 +2,36 @@
 #include <vector>
 
 
-void removeLastN(std::string &str, int n) {    // no-const
-    if (str.length() < n) {
-        return ;
-    }
- 
-    str.erase(str.length() - n);
-}
+class BaseAdventofcode {
+    public:
+    
+        void removeLastN(std::string &str, int n) {    // no-const
+            if (str.length() < n) {
+                return ;
+            }
+        
+            str.erase(str.length() - n);
+        }
 
-void SetPath(){
-	std::string pathname;
+        void SetPath(){
+            std::string pathname;
 
-    pathname = std::filesystem::current_path().string();
-    removeLastN(pathname, 4);
+            pathname = std::filesystem::current_path().string();
+            removeLastN(pathname, 4);
 
-    std::filesystem::current_path(pathname);
-    std::cout << "Path set to: " << pathname << std::endl;
-}
+            std::filesystem::current_path(pathname);
+            std::cout << "Path set to: " << pathname << std::endl;
+        }
 
-std::vector<std::string> TXTtoVector(std::string str){
-	// making vector for storing the data
-	std::vector<std::string> data;
-	std::string line;
-	std::ifstream myfile (str, std::ios::in);
+        std::vector<std::string> TXTtoVector(std::string str){
+            // making vector for storing the data
+            std::vector<std::string> data;
+            std::string line;
+            std::ifstream myfile (str, std::ios::in);
 
-	while(std::getline (myfile,line))
-		data.push_back(line);
-	return data;
-}
+            while(std::getline (myfile,line))
+                data.push_back(line);
+            return data;
+        }
 
-
+};
