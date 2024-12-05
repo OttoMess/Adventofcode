950dc4db07342eed98203f63274dded13d0af9a8
Wouter Broeze <wouter.broeze@demcon.com>
2024-12-05 11:00:52 +0100

    day5 and blank

--- 2024/day5.py
+++ 2024/day5.py
@@ -10,7 +10,8 @@ class Puzzle5:
         start_time = time.time()
 
         self.file_path = path
-        self.input = list()
+        self.order = list()
+        self.manuals = list()
 
         print(self.file_path)
         self.read_txt()
@@ -20,17 +21,48 @@ class Puzzle5:
         print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
 
     def read_txt(self):
-        data = list()
         with open(self.file_path) as file:
             for line in file:
-                data.append(line.strip())
-        self.input = data
+                if "|" in line:
+                    self.order.append(line.strip().split("|"))
+                elif "," in line:
+                    self.manuals.append(line.strip().split(","))
+
+    def valid_manual(self, manual):
+        for i, _ in enumerate(manual):
+            after = [order[1] for order in self.order if order[0] == manual[i]]
+            for j in range(i + 1, len(manual)):
+                e = manual[j]
+                if not manual[j] in after:
+                    return False, [i, j]
+        return True, _
+
+    @staticmethod
+    def find_middle(manual):
+        middle_index = int((len(manual) - 1) / 2)
+        return int(manual[middle_index])
 
     def part1(self):
-        return
+        middle_values = list()
+        for manual in self.manuals:
+            found, _ = self.valid_manual(manual)
+            if found:
+                middle_values.append(self.find_middle(manual))
+        return sum(middle_values)
 
     def part2(self):
-        return
+        middle_values = list()
+        for manual in self.manuals:
+            found, pages = self.valid_manual(manual)
+            if not found:
+                while not found:
+                    manual[pages[0]], manual[pages[1]] = (
+                        manual[pages[1]],
+                        manual[pages[0]],
+                    )
+                    found, pages = self.valid_manual(manual)
+                middle_values.append(self.find_middle(manual))
+        return sum(middle_values)
 
 
 Puzzle5(EXAMPLE)
