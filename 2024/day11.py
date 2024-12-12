e02e3acf5e34e89d4c64a2134b2deb18d32af820
OttoMess <108458116+OttoMess@users.noreply.github.com>
2024-12-12 10:04:00 +0100

    part 2 done

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,5 +1,5 @@
 import time
-
+from heapq import heappush, heappop
 
 EXAMPLE = "AoC_inputs/2024/day_11_example.txt"
 INPUT = "AoC_inputs/2024/day_11.txt"
@@ -13,9 +13,8 @@ class Puzzle11:
         self.input = list()
 
         print(self.file_path)
-        # self.read_txt()
 
-        print(f"output part one: {len(self.part1())}")
+        print(f"output part one: {self.part1()}")
         print(f"output part two: {self.part2()}")
         print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
 
@@ -23,54 +22,45 @@ class Puzzle11:
         with open(self.file_path) as file:
             for line in file:
                 data = [int(i) for i in line.strip().split()]
-        # data = [int(i) for i in line.strip().split()]
         return data
 
     def part1(self):
         stones = self.read_txt()
+        stones_count = {stone: 1 for stone in stones}
         for _ in range(25):
-            stones = self.apply_rules(stones)
-        return stones
+            stones_count = self.blink(stones_count)
+        return sum(stones_count.values())
 
     def part2(self):
-        stones = self.part1()
-        counter = 0
-        i = 1
-        loop_time = time.time()
-        for stone in stones:  # build heap que ?
-            stone = [stone]
-            for i in range(25):
-                stone = self.apply_rules(stone)
-                for st in stone:
-                    st = [st]
-                    for i in range(25):
-                        st = self.apply_rules(st)
-                    counter += len(st)
-            print(f"{i} done of {len(stones)} {round(time.time()-loop_time ,4)}")
-        return len(stones)
+        stones = self.read_txt()
+        stones_count = {stone: 1 for stone in stones}
+        for _ in range(75):
+            stones_count = self.blink(stones_count)
+
+        return sum(stones_count.values())
+
+    def blink(self, stones):
+        new_stones = dict()
+        for stone in stones.keys():
+            blink_stones = self.rules(stone)
+            for new in blink_stones:
+                if new in new_stones.keys():
+                    new_stones[new] += stones[stone]
+                else:
+                    new_stones[new] = stones[stone]
+        return new_stones
 
     @staticmethod
     def rules(stone):
-        stone_string = str(stone)
         if stone == 0:
             return [1]
-        elif len(stone_string) % 2 == 0:
-            mid = len(stone_string) // 2
-            left = int(stone_string[:mid])
-            right = int(stone_string[mid:])
+        elif len(str(stone)) % 2 == 0:
+            mid = len(str(stone)) // 2
+            left = int(str(stone)[:mid])
+            right = int(str(stone)[mid:])
             return [left, right]
         else:
             return [stone * 2024]
-
-    @staticmethod
-    def apply_rules(stones):
-        new_stones = list()
-        for stone in stones:
-            new = Puzzle11.rules(stone)
-            for i in new:
-                new_stones.append(i)
-
-        return new_stones
 
 
 Puzzle11(EXAMPLE)
