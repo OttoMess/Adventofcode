db57d3f0d3bb8845276b60b38c0e0048a4b70e17
OttoMess <108458116+OttoMess@users.noreply.github.com>
2024-12-14 21:29:30 +0100

    day 13 part 1 and 2 in one

--- 2024/day13.py
+++ 2024/day13.py
@@ -15,8 +15,8 @@ class Puzzle13:
         print(self.file_path)
         self.read_txt()
 
-        print(f"output part one: {self.part1()}")
-        print(f"output part two: {self.part2()}")
+        print(f"output part one: {self.part1and2()}")
+        print(f"output part two: {self.part1and2(10000000000000)}")
         print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
 
     def read_txt(self):
@@ -39,27 +39,24 @@ class Puzzle13:
 
         self.input = data
 
-    def part1(self):
-        return
-
-    def part2(self):
+    def part1and2(self, extra: int = 0):
         cost = 0
         for machine in self.input:
-            Px = machine["prize"][0] + 10000000000000
-            Py = machine["prize"][1] + 10000000000000
+            Px = machine["prize"][0] + extra
+            Py = machine["prize"][1] + extra
             Ax = machine["A"][0]
             Ay = machine["A"][1]
             Bx = machine["B"][0]
             By = machine["B"][1]
             An, Bn = sym.symbols("An Bn")
+
             eq1 = -Py + Ay * (Px - Bx * Bn) / Ax + By * Bn
             b_n = sym.solve(eq1, Bn)
+
             eq2 = -An + (Px - Bx * b_n[0]) / Ax
             a_n = sym.solve(eq2, An)
 
-            A_n = int(a_n[0])
-            B_n = int(b_n[0])
-            if A_n > 0 and B_n > 0 and a_n[0] - A_n == 0 and b_n[0] - B_n == 0:
+            if a_n[0] - int(a_n[0]) == 0 and b_n[0] - int(b_n[0]) == 0:
                 cost += a_n[0] * 3 + b_n[0]
         return cost
 
