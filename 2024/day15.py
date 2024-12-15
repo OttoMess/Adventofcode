d33f20a8d537d7b34e8424b86edec35614e6bc4f
OttoMess <108458116+OttoMess@users.noreply.github.com>
2024-12-15 21:28:59 +0100

    part 2 day 15

--- 2024/day15.py
+++ 2024/day15.py
@@ -31,9 +31,9 @@ class Puzzle15:
         self.x_lim = len(data[0])
         self.moves = "".join([i for i in moves])
         self.map = data
+        self.wide_map = self.double_width()
 
     def part1(self):
-        self.read_txt()
         robot = self.start_position()
         for move in self.moves:
             if move == "<":
@@ -46,6 +46,7 @@ class Puzzle15:
                 d = (1, 0)
 
             robot = self.move_robot(robot, d)
+
         counter = 0
         for y, line in enumerate(self.map):
             for x, cha in enumerate(line):
@@ -54,7 +55,26 @@ class Puzzle15:
         return counter
 
     def part2(self):
-        return
+        self.map = self.wide_map
+        robot = self.start_position()
+        for move in self.moves:
+            if move == "<":
+                d = (0, -1)
+            elif move == "^":
+                d = (-1, 0)
+            elif move == ">":
+                d = (0, 1)
+            elif move == "v":
+                d = (1, 0)
+
+            robot = self.move_robot(robot, d)
+
+        counter = 0
+        for y, line in enumerate(self.map):
+            for x, cha in enumerate(line):
+                if cha == "[":
+                    counter += y * 100 + x
+        return counter
 
     def start_position(self):
         for y, line in enumerate(self.map):
@@ -64,48 +84,130 @@ class Puzzle15:
 
     def move_robot(self, robot, d):
         new_robot = [robot[0] + d[0], robot[1] + d[1]]
+        tester = self.map[new_robot[0]][new_robot[1]]
 
-        if self.map[new_robot[0]][new_robot[1]] == ".":
-            self.update_map(robot, ".")
+        box_markers = {"[", "]"}
+        sideways = {(0, 1), (0, -1)}
+
+        if tester == ".":
+            self.update_map(robot)
             self.update_map(new_robot, "@")
             return new_robot
 
-        elif self.map[new_robot[0]][new_robot[1]] == "#":
+        elif tester == "#":
             return robot
 
-        elif self.map[new_robot[0]][new_robot[1]] == "O":
+        elif tester == "O" or tester in box_markers and d in sideways:
             movable = self.move_boxes(new_robot, d)
             if movable:
-                self.update_map(robot, ".")
+                self.update_map(robot)
+                self.update_map(new_robot, "@")
+                return new_robot
+            else:
+                return robot
+
+        elif tester in box_markers and d not in sideways:
+            movable = self.check_wide_box_movable(new_robot, d)
+            if movable:
+                self.move_wide_boxes_up_down(new_robot, d)
+                self.update_map(robot)
                 self.update_map(new_robot, "@")
                 return new_robot
             else:
                 return robot
 
     def move_boxes(self, box, d):
+        box_marker = self.map[box[0]][box[1]]
         new_box = [box[0] + d[0], box[1] + d[1]]
+        tester = self.map[new_box[0]][new_box[1]]
 
-        if self.map[new_box[0]][new_box[1]] == ".":
-            self.update_map(box, ".")
-            self.update_map(new_box, "O")
+        if tester == ".":
+            self.update_map(box)
+            self.update_map(new_box, box_marker)
             return True
 
-        elif self.map[new_box[0]][new_box[1]] == "#":
+        elif tester == "#":
             return False
 
-        elif self.map[new_box[0]][new_box[1]] == "O":
+        else:
             movable = self.move_boxes(new_box, d)
             if movable:
-                self.update_map(box, ".")
-                self.update_map(new_box, "O")
+                self.update_map(box)
+                self.update_map(new_box, box_marker)
                 return True
             else:
                 return False
 
-    def update_map(self, n, string="#"):
+    def update_map(self, n, string="."):
         self.map[n[0]] = self.map[n[0]][: n[1]] + string + self.map[n[0]][n[1] + 1 :]
 
+    def double_width(self):
+        wide_map = list()
+        for y, line in enumerate(self.map):
+            wide_map.append("")
+            for cha in line:
+                if cha == "@":
+                    wide_map[y] += "@."
+                elif cha == "#":
+                    wide_map[y] += "##"
+                elif cha == "O":
+                    wide_map[y] += "[]"
+                elif cha == ".":
+                    wide_map[y] += ".."
+        return wide_map
 
-Puzzle15(TEST)
+    def move_wide_boxes_up_down(self, box, d):
+        node = self.map[box[0]][box[1]]
+        new_one = [box[0] + d[0], box[1] + d[1]]
+        move = [box[0] - d[0], box[1] - d[1]]
+        move_char = self.map[move[0]][move[1]]
+        if node == "]":
+            two = [box[0], box[1] - 1]
+            new_two = [two[0] + d[0], two[1] + d[1]]
+            two_char = "["
+        elif node == "[":
+            two = [box[0], box[1] + 1]
+            new_two = [two[0] + d[0], two[1] + d[1]]
+            two_char = "]"
+
+        elif node == ".":
+            return
+
+        self.move_wide_boxes_up_down(new_one, d)
+        self.move_wide_boxes_up_down(new_two, d)
+
+        self.update_map(box)
+        self.update_map(new_one, node)
+
+        self.update_map(two)
+        self.update_map(new_two, two_char)
+        return
+
+    def check_wide_box_movable(self, box, d):
+        node = self.map[box[0]][box[1]]
+        new_one = [box[0] + d[0], box[1] + d[1]]
+
+        if node == "]":
+            two = [box[0], box[1] - 1]
+            new_two = [two[0] + d[0], two[1] + d[1]]
+        elif node == "[":
+            two = [box[0], box[1] + 1]
+            new_two = [two[0] + d[0], two[1] + d[1]]
+
+        elif node == ".":
+            return True
+
+        elif node == "#":
+            return False
+
+        one_check = self.check_wide_box_movable(new_one, d)
+        two_check = self.check_wide_box_movable(new_two, d)
+        if one_check and two_check:
+            return True
+        else:
+            return False
+
+
+# Puzzle15(TEST)
 Puzzle15(EXAMPLE)
 Puzzle15(INPUT)
