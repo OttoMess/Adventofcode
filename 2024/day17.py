040e49d65d91ee4fb2abf069bdc40f12910b11c7
Wouter Broeze <wouter.broeze@demcon.com>
2024-12-17 19:12:47 +0100

    day 17 part 1 done

--- 2024/day17.py
+++ 2024/day17.py
@@ -1,4 +1,5 @@
 import time
+from matplotlib import pyplot as plt
 
 TEST = "AoC_inputs/2024/day_17_test.txt"
 EXAMPLE = "AoC_inputs/2024/day_17_example.txt"
@@ -16,7 +17,8 @@ class Puzzle17:
         self.read_txt()
 
         print(f"output part one: {self.part1()}")
-        print(f"output part two: {self.part2()}")
+        # print(f"output part two: {self.part2()}")
+        self.find_start_len16()
         print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
 
     def read_txt(self):
@@ -61,8 +63,120 @@ class Puzzle17:
         return value
 
     def part2(self):
+        """
+        2,4,1,5,7,5, 4,3,1,6,0,3,5,5,3,0
+        B = B^4     2,4
+        A = A//B    1,5
+        C = A//B    7,5
+        B = B^C     4,3
+        B = B^6     1,6
+        A = A //3   0,3
+        out=B%5     5,5
+        restart     3,0
         # TODO reverse program ?
-        return
+
+
+        B = B^4     2,4
+        A = A//B^4      1,5
+        C =(A//B^4)//B^4    7,5
+
+        B = B^((A//B^4)//B^4)   4,3
+        B = (B^((A//B^4)//B^4))^6     1,6
+        A = A //3   0,3
+        out=B%5     5,5
+        restart     3,0
+
+
+        """
+        A_start = int(3.5e13)
+        As = list()
+        loops = 0
+        outs = list()
+        while loops < 60:
+            self.out = list()
+            self.pointer = 0
+            self.A = A_start
+            while self.pointer < len(self.program):
+                opcode = self.program[self.pointer]
+                operand = self.program[self.pointer + 1]
+
+                if opcode == 0:
+                    self.opcode_0(operand)
+                elif opcode == 1:
+                    self.opcode_1(operand)
+                elif opcode == 2:
+                    self.opcode_2(operand)
+                elif opcode == 3:
+                    self.opcode_3(operand)
+                elif opcode == 4:
+                    self.opcode_4()
+                elif opcode == 5:
+                    self.opcode_5(operand)
+                elif opcode == 6:
+                    self.opcode_6(operand)
+                elif opcode == 7:
+                    self.opcode_7(operand)
+
+            if self.program == self.out:
+                print(f"found with A = {A_start }")
+                return A_start, self.out, self.program
+            loops += 1
+            outs.append(len(self.out))
+            As.append(A_start)
+            A_start = int(A_start * 1.005)
+        self.plot(As, outs)
+        return A_start, self.out, self.program
+
+    def find_start_len16(self):
+        A_15 = int(3.5184e13)
+        A_16 = int(3.51851e13)
+        As = list()
+        loops = 0
+        outs = list()
+
+        while loops < 600:
+            A_start = (A_16 + A_15) // 2
+            self.out = list()
+            self.pointer = 0
+            self.A = A_start
+            while self.pointer < len(self.program):
+                opcode = self.program[self.pointer]
+                operand = self.program[self.pointer + 1]
+
+                if opcode == 0:
+                    self.opcode_0(operand)
+                elif opcode == 1:
+                    self.opcode_1(operand)
+                elif opcode == 2:
+                    self.opcode_2(operand)
+                elif opcode == 3:
+                    self.opcode_3(operand)
+                elif opcode == 4:
+                    self.opcode_4()
+                elif opcode == 5:
+                    self.opcode_5(operand)
+                elif opcode == 6:
+                    self.opcode_6(operand)
+                elif opcode == 7:
+                    self.opcode_7(operand)
+
+            if self.program == self.out:
+                print(f"found with A = {A_start }")
+                return A_start, self.out, self.program
+            loops += 1
+            outs.append(len(self.out))
+            As.append(A_start)
+            if len(self.out) == 15:
+                A_15 = A_start
+            elif len(self.out) == 16:
+                A_16 = A_start
+        self.plot(As, outs)
+        return A_start, self.out, self.program
+
+    def plot(self, A, outs):
+        # size = [len(o) for o in outs]
+        plt.plot(A, outs)
+        plt.show()
 
     def combo(self, input):
         if input <= 3:
@@ -110,5 +224,5 @@ class Puzzle17:
 
 
 # Puzzle17(TEST)
-Puzzle17(EXAMPLE)
+# Puzzle17(EXAMPLE)
 Puzzle17(INPUT)
