bf47af83f7a91a5ddcf72b182ef01fbcef4790fa
Wouter Broeze <wouter.broeze@demcon.com>
2024-12-18 10:28:09 +0100

    nx.grid_2d_graph, greatly reduces code complexity

--- 2024/day18.py
+++ 2024/day18.py
@@ -1,6 +1,7 @@
 import time
 import networkx as nx
 
+
 EXAMPLE = "AoC_inputs/2024/day_18_example.txt"
 INPUT = "AoC_inputs/2024/day_18.txt"
 
@@ -28,51 +29,27 @@ class Puzzle18:
         self.bytes = data
 
     def part1and2(self):
-        self.clean_memory()
-        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        grid = nx.DiGraph()
-        for y, line in enumerate(self.memory):
-            for x, _ in enumerate(line):
-                node = (y, x)
-                for dirs in directions:
-                    grid.add_node((node, dirs))
+        self.clean_memory()  # only for visual purposes
 
-        for node, dirs in grid.nodes:
-            # adding edge with weight 1 for each node with same dir are start note
-            next_node = (node[0] + dirs[0], node[1] + dirs[1])
-            if (next_node, dirs) in grid.nodes:
-                grid.add_edge((node, dirs), (next_node, dirs), weight=1)
-            # adding the rotation weight
-            if dirs[1] == 0:  # moving up and down, switching to left right
-                grid.add_edge((node, dirs), (node, (0, 1)), weight=0)
-                grid.add_edge((node, dirs), (node, (0, -1)), weight=0)
-            elif dirs[0] == 0:  # moving left and right, switching to up down
-                grid.add_edge((node, dirs), (node, (1, 0)), weight=0)
-                grid.add_edge((node, dirs), (node, (-1, 0)), weight=0)
+        grid = nx.grid_2d_graph(self.map_size, self.map_size)
 
-        start = ((0, 0), (0, 1))
+        start = (0, 0)
         end = (self.map_size - 1, self.map_size - 1)
 
-        # adding edges for the end point
-        for dirs in directions:
-            grid.add_edge((end, dirs), "end", weight=0)
-
         for i in range(len(self.bytes)):
-            self.update_memory(self.bytes[i], "#")
-            for dirs in directions:
-                grid.remove_node((self.bytes[i], dirs))
+            self.update_memory(self.bytes[i])  # only for visual purposes
+            grid.remove_node(self.bytes[i])
 
             if i > self.amount:
                 try:
-                    w = nx.shortest_path_length(grid, start, "end", weight="weight")
+                    w = nx.shortest_path_length(grid, start, end)
                 except:
                     yield self.bytes[i]  # part 2
-            elif i == self.amount:
-                yield nx.shortest_path_length(
-                    grid, start, "end", weight="weight"
-                )  # part 1
+                    break
+            elif i == self.amount - 1:
+                yield nx.shortest_path_length(grid, start, end)  # part 1
 
-    def update_memory(self, n, string="%"):  # visual purposes only
+    def update_memory(self, n, string="#"):  # visual purposes only
         self.memory[n[1]] = (
             self.memory[n[1]][: n[0]] + string + self.memory[n[1]][n[0] + 1 :]
         )
