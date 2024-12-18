980b99ebc075c94ea11466bf53e83aa143d0a770
OttoMess <108458116+OttoMess@users.noreply.github.com>
2024-12-18 22:15:40 +0100

    different approach

--- 2024/day17.py
+++ 2024/day17.py
@@ -12,15 +12,12 @@ class Puzzle17:
         start_time = time.time()
 
         self.file_path = path
-        self.input = list()
 
         print(self.file_path)
         self.read_txt()
 
         print(f"output part one: {self.part1()}")
         print(f"output part two: {self.part2()}")
-        # self.find_start_len16()
-        # self.find_start_len17()
         print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
 
     def read_txt(self):
@@ -38,31 +35,9 @@ class Puzzle17:
                     self.program = [int(i) for i in temp[1].split(",")]
 
     def part1(self):
-        self.out = list()
-        self.pointer = 0
-        while self.pointer < len(self.program):
-            opcode = self.program[self.pointer]
-            operand = self.program[self.pointer + 1]
-
-            if opcode == 0:
-                self.opcode_0(operand)
-            elif opcode == 1:
-                self.opcode_1(operand)
-            elif opcode == 2:
-                self.opcode_2(operand)
-            elif opcode == 3:
-                self.opcode_3(operand)
-            elif opcode == 4:
-                self.opcode_4()
-            elif opcode == 5:
-                self.opcode_5(operand)
-            elif opcode == 6:
-                self.opcode_6(operand)
-            elif opcode == 7:
-                self.opcode_7(operand)
-
-        value = ",".join([str(i) for i in self.out])
-        return value
+        out = self.program_runner(self.program, self.A)
+        stringer = ",".join([str(i) for i in out])
+        return stringer
 
     def part2(self):
         """
@@ -75,181 +50,90 @@ class Puzzle17:
         A = A //3   0,3
         out=B%5     5,5
         restart     3,0
-        # TODO reverse program ?
-
-
-        B = B^4     2,4
-        A = A//B^4      1,5
-        C =(A//B^4)//B^4    7,5
-
-        B = B^((A//B^4)//B^4)   4,3
-        B = (B^((A//B^4)//B^4))^6     1,6
-        A = A //3   0,3
-        out=B%5     5,5
-        restart     3,0
-
-
         """
-        lower_edge = self.find_start_len16()
-        upper_edge = self.find_start_len17()
-
-        search = np.linspace(1.054e14, 1.408e14, 2500)
-
-        output = list()
-        second = list()
-        third = list()
-        input = list()
-        A_start = lower_edge
-        for A in search:
-            # input.append(A)
-            self.program_runner(int(A))
-            temp = "".join([str(j) for j in self.out])
-            output.append(int(temp[-1]))
-            second.append(int(temp[-2]))
-            third.append(int(temp[-3]))
-
-            # A_start = int(A_start * 1.1)
-        plt.plot(search, output)
-        plt.plot(search, second)
-        plt.plot(search, third)
-        plt.show()
+        e = self.program_runner(self.program, self.A)
         return
 
-    def find_start_len16(self):
-        A_low = int(3.5184e13)
-        A_high = int(3.51851e13)
-        while True:
-            A_start = (A_high + A_low) // 2
-            self.program_runner(A_start)
+    @staticmethod
+    def program_runner(program, a):
+        out = list()
+        pointer = 0
+        b = 0
+        c = 0
+        while pointer < len(program):
+            opcode = program[pointer]
+            operand = program[pointer + 1]
+            combo = [0, 1, 2, 3, a, b, c][operand]
 
-            if self.program == self.out:
-                print(f"found with A = {A_start }")
-                return A_start, self.out, self.program
+            match opcode:
+                case 0:
+                    a = int(a / 2**combo)
+                case 1:
+                    b = b ^ operand
+                case 2:
+                    b = combo % 8
+                case 3:
+                    if a != 0:
+                        pointer = operand - 2
+                case 4:
+                    b = b ^ c
+                case 5:
+                    out.append(combo % 8)
+                case 6:
+                    b = int(a / 2**combo)
+                case 7:
+                    c = int(a / 2**combo)
 
-            if len(self.out) == 15:
-                A_low = A_start
-            elif len(self.out) == 16:
-                A_high = A_start
+            pointer += 2
+        return out
 
-            if A_high - A_low < 50:
-                break
 
-        A_start = A_low
-        while True:
-            self.program_runner(A_start)
+# from re import findall
 
-            if len(self.out) == 16:
-                print(f"found start of 16 lengths outputs {A_start}")
-                break
-            else:
-                A_start += 1
+# a, b, c, *prog = [
+#     int(n) for n in findall("(\d+)", open("AoC_inputs/2024/day_17.txt").read())
+# ]
 
-        return A_start
 
-    def find_start_len17(self):
-        A_low = int(2.77e14)
-        A_high = int(3e14)
-        while True:
-            A_start = (A_high + A_low) // 2
-            self.program_runner(A_start)
+# def run(prog, a):
+#     ip, b, c, out = 0, 0, 0, []
+#     while ip >= 0 and ip < len(prog):
+#         lit, combo = prog[ip + 1], [0, 1, 2, 3, a, b, c, 99999][prog[ip + 1]]
+#         match prog[ip]:
+#             case 0:
+#                 a = int(a / 2**combo)  # adv
+#             case 1:
+#                 b = b ^ lit  # bxl
+#             case 2:
+#                 b = combo % 8  # bst
+#             case 3:
+#                 ip = ip if a == 0 else lit - 2  # jnz
+#             case 4:
+#                 b = b ^ c  # bxc
+#             case 5:
+#                 out.append(combo % 8)  # out
+#             case 6:
+#                 b = int(a / 2**combo)  # bdv
+#             case 7:
+#                 c = int(a / 2**combo)  # cdv
+#         ip += 2
+#     return out
 
-            if self.program == self.out:
-                print(f"found with A = {A_start }")
-                return A_start, self.out, self.program
 
-            if len(self.out) == 16:
-                A_low = A_start
-            elif len(self.out) == 17:
-                A_high = A_start
+# print("Part 1:", ",".join(str(n) for n in run(prog, a)))
 
-            if A_high - A_low < 50:
-                break
+# target = prog[::-1]
 
-        A_start = A_low
-        while True:
-            self.program_runner(A_start)
 
-            if len(self.out) == 17:
-                print(f"found end of 16 lengths outputs {A_start-1}")
-                break
-            else:
-                A_start += 1
-
-        return A_start - 1
-
-    def program_runner(self, A_start):
-        self.out = list()
-        self.pointer = 0
-        self.A = A_start
-        while self.pointer < len(self.program):
-            opcode = self.program[self.pointer]
-            operand = self.program[self.pointer + 1]
-
-            if opcode == 0:
-                self.opcode_0(operand)
-            elif opcode == 1:
-                self.opcode_1(operand)
-            elif opcode == 2:
-                self.opcode_2(operand)
-            elif opcode == 3:
-                self.opcode_3(operand)
-            elif opcode == 4:
-                self.opcode_4()
-            elif opcode == 5:
-                self.opcode_5(operand)
-            elif opcode == 6:
-                self.opcode_6(operand)
-            elif opcode == 7:
-                self.opcode_7(operand)
-
-    def plot(self, A, outs):
-        # size = [len(o) for o in outs]
-        plt.plot(A, outs)
-        plt.show()
-
-    def combo(self, input):
-        if input <= 3:
-            return input
-        elif input == 4:
-            return self.A
-        elif input == 5:
-            return self.B
-        elif input == 6:
-            return self.C
-
-    def opcode_0(self, input):
-        self.A = self.A // 2 ** self.combo(input)
-        self.pointer += 2
-
-    def opcode_1(self, input):
-        self.B = self.B ^ input
-        self.pointer += 2
-
-    def opcode_2(self, input):
-        self.B = self.combo(input) % 8
-        self.pointer += 2
-
-    def opcode_3(self, input):
-        if self.A == 0:
-            self.pointer += 2
-        else:
-            self.pointer = input
-
-    def opcode_4(self):
-        self.B = self.B ^ self.C
-        self.pointer += 2
-
-    def opcode_5(self, input):
-        self.out.append(self.combo(input) % 8)
-        self.pointer += 2
-
-    def opcode_6(self, input):
-        self.B = self.A // 2 ** self.combo(input)
-        self.pointer += 2
-
-    def opcode_7(self, input):
-        self.C = self.A // 2 ** self.combo(input)
-        self.pointer += 2
+# def find_a(a=0, depth=0):
+#     if depth == len(target):
+#         return a
+#     for i in range(8):
+#         output = run(prog, a * 8 + i)
+#         if output and output[0] == target[depth]:
+#             if result := find_a((a * 8 + i), depth + 1):
+#                 return result
+#     return 0
 
 
 # Puzzle17(TEST)
