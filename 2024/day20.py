b7de18c74d96ef6aa8b437e9dfc851a6b521fcff
Wouter Broeze <wouter.broeze@demcon.com>
2024-12-20 16:40:35 +0100

    redo of day 2

--- 2024/day20.py
+++ 2024/day20.py
@@ -36,64 +36,48 @@ class Puzzle20:
                         self.end = (y, x)
 
         self.grid = nx.grid_2d_graph(depth + 1, width + 1)
-        self.walls = list()
+        self.inner_walls = list()
         for wall in walls:
             self.grid.remove_node(wall)
             if 0 < wall[0] < depth and 0 < wall[1] < width:
-                self.walls.append(wall)
-
-        # plt.figure(figsize=(6, 6))
-        # pos = {(x, y): (y, -x) for x, y in self.grid.nodes()}
-        # nx.draw(
-        #     self.grid, pos=pos, node_color="lightgreen", with_labels=True, node_size=600
-        # )
-        # plt.show()
+                self.inner_walls.append(wall)
         return
 
     def part1(self):
-        ref = nx.shortest_path(self.grid, self.start, self.end)
+        ref = nx.shortest_path(self.grid, self.start, self.end)  # only 1 shortest path
         cheat_times = list()
-        short_cuts = self.next_to_path(ref)
-        for wall in self.walls:
-            # TODO only check the wall adjacent to shortest path, are there more than 1 shortest path ?
-            edges = self.edge_finder(wall)
-            self.grid.add_node(wall)
-            self.grid.add_edges_from(edges)
-            t = nx.shortest_path_length(self.grid, self.start, self.end)
-            if t < len(ref) - 100:
-                cheat_times.append((len(ref)) - t)
-            self.grid.remove_node(wall)
+        lookup = set(ref)
+        for wall in self.inner_walls:
+            possible, next, prev = self.short_cut_possible(lookup, wall)
+            if possible:
+                a = ref.index(next)
+                b = ref.index(prev)
+                time_saved = abs(a - b) - 2  # for steps taken in shortcut
+
+                if time_saved >= 100:
+                    cheat_times.append(time_saved)
+
         return len(cheat_times)
 
     def part2(self):
         return
 
-    def edge_finder(self, node):
-        adjacent: list = list()
-        y = node[0]
-        x = node[1]
-        steps = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        for step in steps:
-            next = (y + step[0], x + step[1])
-            if next in self.grid.nodes:
-                adjacent.append((node, next))
-        return adjacent
+    @staticmethod
+    def short_cut_possible(path, wall):
+        y = wall[0]
+        x = wall[1]
 
-    def next_to_path(self, path):
-        path = set(path)
-        steps = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        short_cut = list()
-        for wall in self.walls:
-            count = 0
-            y = wall[0]
-            x = wall[1]
-            for step in steps:
-                next = (y + step[0], x + step[1])
-                if next in path:
-                    count += 1
-            if count >= 2:
-                short_cut.append(wall)
-        return short_cut
+        prev = (y + 1, x)
+        next = (y + -1, x)
+        if next in path and prev in path:
+            return True, prev, next
+
+        prev = (y, x - 1)
+        next = (y, x + 1)
+        if next in path and prev in path:
+            return True, prev, next
+
+        return False, None, None
 
     def plot_nodes(self):
         plt.figure(figsize=(6, 6))
