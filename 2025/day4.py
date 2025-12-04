f10fbbde07be80f2a763ed70054522c2fe09638a
OttoMess <108458116+OttoMess@users.noreply.github.com>
2025-12-04 07:34:02 +0100

    add day4

---
+++ 2025/day4.py
@@ -0,0 +1,37 @@
+import time
+
+
+EXAMPLE = "AoC_inputs/2025/day_4_example.txt"
+INPUT = "AoC_inputs/2025/day_4.txt"
+
+
+class Puzzle4:
+    def __init__(self, path):
+        start_time = time.time()
+
+        self.file_path = path
+        self.input = list()
+
+        print(self.file_path)
+        self.read_txt()
+
+        print(f"output part one: {self.part1()}")
+        print(f"output part two: {self.part2()}")
+        print(f"Run time {round(time.time() - start_time, 4)} [sec]\n")
+
+    def read_txt(self):
+        data = list()
+        with open(self.file_path) as file:
+            for line in file:
+                data.append(line.strip())
+        self.input = data
+
+    def part1(self):
+        return
+
+    def part2(self):
+        return
+
+
+Puzzle4(EXAMPLE)
+Puzzle4(INPUT)
