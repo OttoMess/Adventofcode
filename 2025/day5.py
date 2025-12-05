4b50565b9bf26acc0c40bdff160702b1e4e364f6
OttoMess <hennek12@gmail.com>
2025-12-05 14:34:25 +0100

    progess commit

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,5 +1,5 @@
 import time
-import numpy as np
+import intervaltree
 
 EXAMPLE = "AoC_inputs/2025/day_5_example.txt"
 INPUT = "AoC_inputs/2025/day_5.txt"
@@ -32,8 +32,9 @@ class Puzzle5:
                 if next:
                     ingredients.append(int(line.strip()))
                 else:
-                    fresh.append(tuple([int(i) for i in line.strip().split("-")]))
-        
+                    fresh.append(tuple([int(i)
+                                 for i in line.strip().split("-")]))
+
         self.fresh = tuple(fresh)
         self.ingredients = tuple(ingredients)
 
@@ -42,11 +43,67 @@ class Puzzle5:
         for ingredient in self.ingredients:
             for start, end in self.fresh:
                 if ingredient >= start and ingredient <= end:
-                    counter+=1
+                    counter += 1
                     break
         return counter
 
     def part2(self):
+        condensed = list()
+        full_range = list()
+        loop = 0
+        # ranger = list()
+        intervals = list()
+        # tree = intervaltree.IntervalTree()
+        for begin, end in self.fresh:
+            # ranger.append(range(begin, end))
+            intervals.append([begin, end])
+
+        intervals.sort()
+        residue = list()
+        residue.append(intervals[0])
+
+        for i in range(1, len(intervals)):
+            last = residue[-1]
+            curr = intervals[i]
+
+            # If current interval overlaps with the last merged
+            # interval, merge them
+            if curr[0] <= last[1]:
+                last[1] = max(last[1], curr[1])
+            else:
+                residue.append(curr)
+
+        #     if begin != end:
+        #         tree[begin: end] = (begin, end)
+        #         # tree = it.IntervalTree.from_tuples(self.fresh)
+        # tree.merge_overlaps(strict=False)
+        # for begin, end in self.fresh:
+        #     if loop == 0:
+        #         condensed.append([begin, end, True])
+        #         full_range = [begin, end]
+        #     else:
+        #         if begin > full_range[1]:  # expand range positive side
+        #             condensed.append([full_range[1]+1, begin-1, False])
+        #             full_range[1] = end
+        #             condensed.append([begin, end, True])
+
+        #         elif end < full_range[0]:  # expand range negative  side
+        #             condensed.append([end, full_range[0]-1, False])
+        #             full_range[0] = begin
+        #             condensed.append([begin, end, True])
+
+        #         elif full_range[0] >= begin and end <= full_range[1]:
+        #             q = 1
+
+        # # fit in range already
+        # if a<=begin <=b and a<=end <=b:
+        #     continue
+        # elif a<begin:
+        #     condensed.append([end+1])
+        #     condensed.append([begin,end,True])
+
+        loop += 1
+        # fresh_set.update(range(start, end+1)) #BUG will not work, way to big
         return
 
 
