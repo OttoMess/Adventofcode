c4379e548d27b962acd7cd943265f1df3808c034
OttoMess <hennek12@gmail.com>
2025-12-13 10:02:58 +0100

    further with part 2

--- 2025/day9.py
+++ 2025/day9.py
@@ -10,7 +10,10 @@ class Puzzle9:
         start_time = time.time()
 
         self.file_path = path
-        self.input = list()
+        self.input = []
+        self.combinations = []
+        self.lines = []
+        self.areas = []
 
         print(self.file_path)
         self.read_txt()
@@ -28,40 +31,69 @@ class Puzzle9:
         self.input = tuple(data)
 
     def part1(self) -> int:
-        combinations = [list(x) for x in itertools.combinations(self.input, 2)]
-        max_area = 0
-        for a, b in combinations:
+        self.combinations = tuple([tuple(list(x))
+                                   for x in itertools.combinations(self.input, 2)])
+        area_list = []
+        for i, [a, b] in enumerate(self.combinations):
             height = abs(a[0] - b[0]) + 1
             length = abs(a[1]-b[1]) + 1
-            area = height * length
+            area_list.append([height * length, i])
 
-            if area > max_area:
-                max_area = area
+        self.areas = sorted(area_list, reverse=True)
+        return self.areas[0][0]
 
-        return max_area
+    def valid_area(self, index) -> bool:
+        a, b = self.combinations[index]
+        x_high = max(a[0], b[0])
+        y_high = max(a[1], b[1])
+        x_low = min(a[0], b[0])
+        y_low = min(a[1], b[1])
+
+        for start, end in self.lines:
+
+            if start == a or start == b or end == a or end == b:
+                continue
+
+            # if there is a point of the contour within the area the area is invalid.
+            # excluding the edges, there a point of the contour is possible
+            elif x_low < start[0] < x_high and y_low < start[1] < y_high:
+                return False
+
+            # start is on the edge of the area but the end is pointed inside the area
+            # TODO check if direction of the line is relevant for this code
+            # BUG not Working correctly.
+            elif x_low == start[0] and end[0] > x_low:
+                return False
+
+            elif x_high == start[0] and end[0] < x_high:
+                return False
+
+            elif y_low == start[1] and end[1] > y_low:
+                return False
+
+            elif y_high == start[1] and end[1] < y_high:
+                return False
+
+        return True
 
     def part2(self) -> int:
-        field_width = max([i[0] for i in self.input])+2
-        field_height = max([i[1] for i in self.input])+2
-        field = []
-        for _ in range(field_height):
-            row = ""
-            for _ in range(field_width):
-                row += "."
-            field.append(row)
+        self.lines = []
+        for i, _ in enumerate(self.input):
+            j = (i+1) % len(self.input)
+            self.lines.append((self.input[i], self.input[j]))
 
-        for i in range(len(self.input)):
-            start = self.input[i]
-            end = self.input[(i+1) % len(self.input)]
+        for area, index in self.areas:
+            if index == 23:
+                stop = True
 
-            row = start[0]
-            column = start[1]
-
-            field[start[1]] = field[start[1]][:start[0]] + \
-                "#" + field[start[1]][start[0]+1:]
+            valid = self.valid_area(index)
+            if valid:
+                return area
 
         return 0
 
 
-# Puzzle9(EXAMPLE)
+Puzzle9(EXAMPLE)
 Puzzle9(INPUT)
+
+# 4629504600 to high
