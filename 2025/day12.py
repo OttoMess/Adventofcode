b06e15010980f03e757392068c5d716b4cfcccb1
OttoMess <hennek12@gmail.com>
2025-12-15 20:03:12 +0100

    start on getting all data correctly loaded

--- 2025/day12.py
+++ 2025/day12.py
@@ -23,8 +23,42 @@ class Puzzle12:
         data = list()
         with open(self.file_path) as file:
             for line in file:
+                # make the puzzle pieces with 1 to 5 numbers, for visualization
                 data.append(line.strip())
+
+        shapes = {}
+        begin = 0
+        while begin < 28:
+            end = begin + 5
+            base = [["." for _ in range(3)] for _ in range(3)]
+            coordinates = []
+            for i in range(begin, end):
+                if i % 5 == 0:
+                    marker = int(data[i][0])
+                else:
+                    for j, c in enumerate(data[i]):
+                        if c == "#":
+                            base[(i % 5)-1][j] = marker
+                            coordinates.append([(i % 5)-1, j])
+            shapes[marker] = base
+            begin += 5
+        self.shapes = shapes
+
+        for i in range(30, len(data)+1):
+            a, b = data[i].split(":")
+            size = [int(x) for x in a.split("x")]
+            pieces = [int(x) for x in b.split()]
         self.input = data
+
+    @staticmethod
+    def rotate_shape(shape) -> list:
+        """
+        turn can be 
+
+        :param shape: input list of shape to be turned
+        """
+        turned_shape = []
+        return turned_shape
 
     def part1(self) -> int:
         return 0
